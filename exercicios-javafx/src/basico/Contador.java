5e1ea4c3f6e9ff25f7f812ae6b7a0492da008503
Amonvix <hspedroso@gmail.com>
2023-05-10 18:05:23 -0300

    Atualizado os arquivos do FX

--- exercicios-javafx/src/basico/Contador.java
+++ exercicios-javafx/src/basico/Contador.java
@@ -13,25 +13,38 @@ public class Contador extends Application {
 
 	private int cont = 0;
 
+	private void atualizarCor(Label label) {
+		label.setText(Integer.toString(cont));
+		label.getStyleClass().remove("verde");
+		label.getStyleClass().remove("vermelho");
+
+		if (cont > 0) {
+			label.getStyleClass().add("verde");
+		} else if(cont < 0){
+			label.getStyleClass().add("vermelho");
+		}
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		Label labelTitulo = new Label("Contador");
 		Label labelNumero = new Label("0");
-		labelTitulo.getStyleClass().add("numero");
+		labelTitulo.getStyleClass().add("titulo");
 		labelNumero.getStyleClass().add("numero");
 
 		Button botaoInc = new Button(" + ");
 		Button botaoDec = new Button(" - ");
-
+		botaoInc.getStyleClass().add("botaoInc");
+		botaoDec.getStyleClass().add("botaoDec");
 		botaoInc.setOnAction(e -> {
 			cont++;
-			labelNumero.setText(Integer.toString(cont));
+			atualizarCor(labelNumero);
 		});
 
 		botaoDec.setOnAction(e -> {
 			cont--;
-			labelNumero.setText(Integer.toString(cont));
+			atualizarCor(labelNumero);
 		});
 
 		HBox boxBotoes = new HBox();
@@ -47,12 +60,11 @@ public class Contador extends Application {
 		boxConteudo.getChildren().add(labelTitulo);
 		boxConteudo.getChildren().add(labelNumero);
 		boxConteudo.getChildren().add(boxBotoes);
-		
+
 		String caminhoDoCss = getClass().getResource("/basico/Contador.css").toExternalForm();
 		Scene cenaPrincipal = new Scene(boxConteudo, 300, 300);
 		cenaPrincipal.getStylesheets().add(caminhoDoCss);
 		cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css2?family=Oswald");
-		
 
 		primaryStage.setScene(cenaPrincipal);
 		primaryStage.show();
